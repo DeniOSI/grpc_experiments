88ca009476db39122b15abfd5c9cc33899cb5680
Denys <denys.osypenko@sap.com>
2022-06-05 02:30:20 +0300

    Init add a grpC-server and a grpC-client

---
+++ GrpcGreeter/Services/GreeterService.cs
@@ -0,0 +1,27 @@
+using Grpc.Core;
+using GrpcGreeter;
+
+namespace GrpcGreeter.Services;
+
+public class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<PerfectUser> PerfectWeight(UserData request, ServerCallContext context)
+    {
+        return Task.FromResult( new PerfectUser {PerfectWeight = request.Height - request.Weight});
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+}
---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+using var chanel = GrpcChannel.ForAddress("https://localhost:7285");
+var client = new Greeter.GreeterClient(chanel);
+var response = await client.SayHelloAsync(new HelloRequest
+{
+    Name = "just Test"
+});
+
+Console.WriteLine(response.Message);
+
+var weight = client.PerfectWeight(new UserData{Height = 150, Weight = 60});
+Console.WriteLine(weight.PerfectWeight);
